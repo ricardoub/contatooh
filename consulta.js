7fcc58cf9a6accd0834657d9c90f3b95487a0511
Ricardo Bomfim <ricardoub@gmail.com>
2017-08-15 13:59:30 -0300

    install: mongo, mongoose

---
+++ consulta.js
@@ -0,0 +1,15 @@
+var	MongoClient	=	require('mongodb').MongoClient;
+var	ObjectID	=	require('mongodb').ObjectID;
+//	ObjectID	de	algum	contato	existente
+var	_idProcurado	=	new	ObjectID('5992f9df2bae814e10c467f7');
+MongoClient.connect('mongodb://127.0.0.1:27017/contatooh',	
+	function(erro,	db)	{
+		if(erro)	throw	err;								
+		db.collection('contatos').findOne({_id	:	_idProcurado},	
+			function(erro,	contato)	{
+				if(erro)	throw	err;
+				console.log(contato);
+			}
+		);				
+	}
+);
