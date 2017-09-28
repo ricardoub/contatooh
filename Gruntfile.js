b69cd981ee85941e75e7cd0a7cb08094f145e2f4
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-28 14:22:24 -0300

    pg277 - grunt usemin

--- Gruntfile.js
+++ Gruntfile.js
@@ -7,27 +7,42 @@ module.exports = function(grunt) {
         cwd: '.',
         src: [
           '**',
-          '!Gruntfile.js', '!package.json', '!bower.json',
-          '!node_modules'
+          '!Gruntfile.js', '!package.json', '!bower.json'
         ],
         dest: 'dist'
       }
     },
+
     clean: {
       dist: {
         src: 'dist'
       }
+    },
+
+    usemin: {
+      html: 'dist/app/views/**/*.ejs'
+    },
+
+    useminPrepare: {
+      options: {
+        root: 'dist/public',
+        dest: 'dist/public'
+      },
+      html: 'dist/app/views/**/*ejs'
     }
+
   });
 
-  grunt.registerTask('default', ['dist']);
+  grunt.registerTask('default', ['dist', 'minifica']);
   grunt.registerTask('dist', ['clean', 'copy']);
+  grunt.registerTask('minifica', ['useminPrepare', 'concat', 'uglify', 'cssmin', 'usemin']);
+
 
   grunt.loadNpmTasks('grunt-contrib-copy');
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-contrib-cssmin');
   grunt.loadNpmTasks('grunt-usemin');
   
 };
