39efd469c93f0d29b95adb79304d699cb4c06bf0
Ricardo Bomfim <ricardoub@gmail.com>
2017-09-28 14:12:58 -0300

    pg273 - grunt build

--- Gruntfile.js
+++ Gruntfile.js
@@ -27,6 +27,7 @@ module.exports = function(grunt) {
   grunt.loadNpmTasks('grunt-contrib-clean');
   grunt.loadNpmTasks('grunt-contrib-concat');
   grunt.loadNpmTasks('grunt-contrib-uglify');
-  grunt.loadNpmTasks('grunt-contrib-cssmin');
+  grunt.loadNpmTasks('grunt-contrib-clean');
+  grunt.loadNpmTasks('grunt-usemin');
   
 };
